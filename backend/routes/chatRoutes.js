1bafc23da9ad4838ce0c9fa8bd4619155a9a0b21
parangat11 <parangataditya001@gmail.com>
2025-08-18 22:26:28 +0530

    Create the createGroupChat API

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const { protect } = require("../middleware/authMiddleware");
-const { accessChat, fetchChats } = require("../controllers/chatControllers");
+const {
+    accessChat,
+    fetchChats,
+    createGroupChat,
+} = require("../controllers/chatControllers");
 const router = express.Router();
 
 router.route("/").post(protect, accessChat);
 router.route("/").get(protect, fetchChats);
-// router.route("/group").post(protect, createGroupChat);
+router.route("/group").post(protect, createGroupChat);
 // router.route("/rename").put(protect, renameGroup);
 // router.route("/group-remove").put(protect, removeFromGroup);
 // router.route("/group-add").put(protect, addToGroup);

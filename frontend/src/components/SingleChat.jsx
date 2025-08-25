eb109345fcdd1617f7e699bcfd5eda1c0188b283
parangat11 <parangataditya001@gmail.com>
2025-08-25 22:38:10 +0530

    Add chat box UI separation

--- frontend/src/components/SingleChat.jsx
+++ frontend/src/components/SingleChat.jsx
@@ -46,6 +46,17 @@ const SingleChat = ({ fetchAgain, setFetchAgain }) => {
                             </>
                         )}
                     </Text>
+                    <Box
+                        display="flex"
+                        flexDir="column"
+                        justifyContent="flex-end"
+                        p={3}
+                        bg="#E8E8E8"
+                        w="100%"
+                        h="100%"
+                        borderRadius="lg"
+                        overflowY="hidden"
+                    ></Box>
                 </>
             ) : (
                 <Box

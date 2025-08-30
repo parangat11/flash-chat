e2142c694ee5fb40e0cef50c20a978745bf1ed8b
parangat11 <parangataditya001@gmail.com>
2025-08-31 03:47:16 +0530

    Add origin for sockets

--- backend/server.js
+++ backend/server.js
@@ -42,7 +42,7 @@ const server = app.listen(PORT, console.log(`Server running on port ${PORT}`));
 const io = require("socket.io")(server, {
     pingTimeout: 60000,
     cors: {
-        origin: "http://localhost:5173",
+        origin: allowedOrigins,
     },
 });
 

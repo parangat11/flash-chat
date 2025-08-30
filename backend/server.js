effd87b506f0782952f54e31ac6f4c65b8c0dfb7
parangat11 <parangataditya001@gmail.com>
2025-08-31 05:13:17 +0530

    Add deployment code to the app

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,13 @@ const chatRoutes = require("./routes/chatRoutes.js");
 const userRoutes = require("./routes/userRoutes.js");
 const messageRoutes = require("./routes/messageRoutes.js");
 const dotenv = require("dotenv");
+const path = require("path");
 const connectDB = require("./config/db.js");
 
 const { notFound, errorHandler } = require("./middleware/errorMiddleware.js");
 
 const app = express();
+
 app.use(express.json());
 const cors = require("cors");
 
@@ -16,13 +18,27 @@ app.use(cors());
 dotenv.config();
 connectDB();
 
-app.get("/", (req, res) => {
-    res.send("API is running");
-});
-
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
+
+// Deploment code start
+
+const __dirname1 = path.resolve();
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname1, "/frontend/dist")));
+    app.get("*", (req, res) => {
+        res.sendFile(
+            path.resolve(__dirname1, "frontend", "build", "index.html")
+        );
+    });
+} else {
+    app.get("/", (req, res) => {
+        res.send("API is running");
+    });
+}
+
+//Deployment code end
 
 app.use(notFound);
 app.use(errorHandler);

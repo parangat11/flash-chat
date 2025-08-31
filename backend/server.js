656aa7785a39153cdf8860fc4588a489004020c8
parangat11 <parangataditya001@gmail.com>
2025-08-31 05:35:58 +0530

    Add regex for express v5

--- backend/server.js
+++ backend/server.js
@@ -27,10 +27,8 @@ app.use("/api/message", messageRoutes);
 const __dirname1 = path.resolve();
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname1, "/frontend/dist")));
-    app.get("*", (req, res) => {
-        res.sendFile(
-            path.resolve(__dirname1, "frontend", "dist", "index.html")
-        );
+    app.get(/(.*)/, (req, res) => {
+        res.sendFile(path.join(__dirname1, "frontend/dist", "index.html"));
     });
 } else {
     app.get("/", (req, res) => {
